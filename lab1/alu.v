52f04a79d13c0d3b4588a3bb94803d5d7b185ecf
Ian Chen <ianre657@gmail.com>
2017-03-16 01:33:33 +0800

    opcode done

--- lab1/alu.v
+++ lab1/alu.v
@@ -32,7 +32,6 @@ reg             cout;
 reg             overflow;
 
 
-reg    [1:0]    opcode;
 
 parameter ALU_AND  = 4'b0000;
 parameter ALU_OR   = 4'b0001;
@@ -47,12 +46,13 @@ parameter ZERO_1   = 1'b1;
 
 
 wire [32-1:0] aInvert, bInvert;
+wire [2-1:0]  opcode;
+wire  first_alu_cin;//Used in substraction or SLT
+wire  last_alu_cout;//Used in SLT
 
-assign aInvert = ALU_control == ALU_NOR ?  
-
-
-assign bInvert = ~src2;
-
+assign aInvert = ( (ALU_control == ALU_NOR ) || ( ALU_control == ALU_NAND ) ) ? 1'b1 : 1'b0;
+assign bInvert = ( (ALU_control == ALU_SUB ) || ( ALU_control == ALU_NOR ) || ( ALU_control == ALU_NAND )) ? 1'b1 : 1'b0;
+assign first_alu_cin = ( (ALU_control == ALU_SUB ) || ( ALU_control == ALU_SLT ) ) ? 1'b1 : 1'b0;
 
 //operation code translation
 /*
@@ -67,21 +67,21 @@ assign bInvert = ~src2;
                cout,       //1 bit carry out(output)
 */
 
-//Need 32 instance 
-//alu_top AL1( .src1(), .src2(), .less(), .A_invert(), .B_invert(), .cin(), .operation(), .result(), .cout() ); 
 
-always @(*)begin
-    case( ALU_control )
-        ALU_AND:  opcode = ALU_AND[3: 4];
-        ALU_OR:   opcode = ALU_OR [3: 4];
-        ALU_ADD:  opcode = ALU_ADD[3: 4];
-        ALU_SUB:  opcode = ALU_ADD[3: 4];
-        ALU_NOR:  opcode = ALU_AND[3: 4];
-        ALU_NAND: opcode = ALU_OR [3: 4];
-        ALU_SLT:  opcode = ALU_ADD[3: 4];
-        default: opcode = 2'b0; 
-    endcase 
-end
+
+/* 
+  wire co0, co1, co2, co3, co4, co5, co6, co7, co08, co09, co10, co11, co12, co13, co14, co15, co16,
+       co17, co18, co19, co20, co21, co22, co23, co24, co25, co26, co27, co28, co29, co30, co31;
+*/
+//Need 32 instance 
+//alu_top AL1( .src1(src1[0]), .src2(src2[0]), .less(), .A_invert(), .B_invert(), .cin(), .operation(), .result(), .cout() ); 
+
+//alu_top AL1( .src1(src1[0]), .src2(src2[0]), .less(ZERO_1), .A_invert(aInvert[1]), .B_invert(bInvert[1]), .cin(co0), .operation(opcode), .result(result[1]), .cout(co1) ); 
+
+assign opcode = ( (ALU_control==ALU_AND) || (ALU_control==ALU_NOR)  )? ALU_AND[3:4] :
+                ( (ALU_control==ALU_OR ) || (ALU_control==ALU_NAND) )? ALU_OR [3:4] :
+                ( (ALU_control==ALU_ADD) || (ALU_control==ALU_SUB) || (ALU_control==ALU_SLT) )? ALU_ADD[3:4] :
+                2'bxx;
 
 
 
