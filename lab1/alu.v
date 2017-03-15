c731c68df7bf2d8174128179c1bf4d576f2adf46
Ian Chen <ianre657@gmail.com>
2017-03-15 21:55:35 +0800

    opUnit I/O definition

--- lab1/alu.v
+++ lab1/alu.v
@@ -1,1 +1,24 @@
+module alu(
+    output [31:0]result, 
+    output [31:0] result,
+    output zero,
+    output cout,
 
+    input rst_n,
+    input [31:0] a, b,
+    input [3:0] ALU_control,
+);
+/*
+output wire result,
+    output wire carryout,
+    input wire [31: 0]a,b,
+    input wire [3:0] opcode,
+    input wire carryin,
+    input wire less, 
+*/
+
+
+opUnit u1( );
+
+
+endmodule
--- lab1/opUnit.v
+++ lab1/opUnit.v
@@ -1,19 +1,42 @@
 module opUnit(
-    output wire result,
-    output wire carryout,
-    input wire [31:0]a,b,
-    input wire carryin,
-    input wire less, 
+    output result,
+    output carryout,
+
+    input a,b,
+    input aInvert,bInvert,
+    input [1:0] opcode,
+    input carryin,
+    input less
 );
 
+parameter ALU_AND  = 2'b00;
+parameter ALU_OR   = 2'b01;
+parameter ALU_ADD  = 2'b10;
 
-parameter ALU_AND = 4'b0000;
-parameter ALU_OR  = 4'b0001;
-parameter ALU_ADD = 4'b0010;
-parameter ALU_SUB = 4'b0110;
-parameter ALU_NOR = 4'b1100;
-parameter ALU_NAND = 4'b1101;
-parameter ALU_SLT = 4'b0111;
+wire a_in ,b_in ;
+
+assign a_in = aInvert == 1'b1? ~a : a ;
+assign b_in = bInvert == 1'b1? ~b : b ;
+
+/*
+  assign result = opcode == ALU_AND ? a&b :
+                  opcode == ALU_OR  ? a|b :
+                  opcode == ALU_ADD ? a|  b | carryin :
+                  opcode == ALU_SUB ? a| ~b | carryin :
+                  opcode == ALU_NOR ? ~( a|b):
+                  opcode == ALU_NAND ? ~(a&b):
+                  opcode == ALU_SLT ? 
+ */
+
+
+ /*
+ 從opcode 決定 result 是哪個
+ */
+
+
+  /*
+  assign less
+  */
 
 
 endmodule
