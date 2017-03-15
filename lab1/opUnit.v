923c56198443fdeb493024113b40eaf4ad6a365e
Ian Chen <ianre657@gmail.com>
2017-03-15 18:48:57 +0800

    opeUnit parameter

--- lab1/opUnit.v
+++ lab1/opUnit.v
@@ -6,4 +6,12 @@ module opUnit(
     input wire less, 
 );
 
+parameter ALU_AND = 4'b0000;
+parameter ALU_OR  = 4'b0001;
+parameter ALU_ADD = 4'b0010;
+parameter ALU_SUB = 4'b0110;
+parameter ALU_NOR = 4'b1100;
+parameter ALU_NAND = 4'b1101;
+parameter ALU_SLT = 4'b0111;
+
 endmodule
