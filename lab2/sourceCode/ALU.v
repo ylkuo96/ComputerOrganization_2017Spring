f0c8ea7c31b2dedbac31264427e1f02f8230c00a
Ian Chen <ianre657@gmail.com>
2017-04-25 01:04:05 +0800

    done without debug

--- lab2/sourceCode/ALU.v
+++ lab2/sourceCode/ALU.v
@@ -13,25 +13,68 @@ module ALU(
     src1_i,
 	src2_i,
 	ctrl_i,
+	shamt,
+
 	result_o,
 	zero_o
+	
 	);
      
 //I/O ports
 input  [32-1:0]  src1_i;
 input  [32-1:0]	 src2_i;
 input  [4-1:0]   ctrl_i;
-
+input  [5-1:0]   shamt; 
 output [32-1:0]	 result_o;
 output           zero_o;
 
 //Internal signals
-reg    [32-1:0]  result_o;
-wire             zero_o;
+reg    [32-1:0]  		result_o;
+wire             		zero_o;
+wire signed  [32-1:0]	signed_src1,
+					 	signed_src2;
 
+//Values are implicitly unsigned
+wire  		 [32-1:0]	unsigned_src1,
+						unsigned_src2;
 //Parameter
+parameter ALU_AND  = 4'd0;
+parameter ALU_OR   = 4'd1;
+parameter ALU_ADD  = 4'd2;
+parameter ALU_SUB  = 4'd3;
+parameter ALU_SLT  = 4'd4;
+parameter ALU_SLTU = 4'd5;
+parameter ALU_BNE  = 4'd6;
+parameter ALU_SLL  = 4'd7;
+parameter ALU_SLLV = 4'd8;
+parameter ALU_LUI  = 4'd9;
+parameter ALU_ORI  = 4'd10;
+
 
 //Main function
+assign zero_o = (result_0 ==0) ? 1'b1 :1'b0 ;
+assign signed_src1 = src1_i;
+assign signed_src2 = src2_i;
+assign unsigned_src1 = src1_i;
+assign unsigned_src2 = src2_i;
+
+always @( *)begin
+  case (ctrl_i)
+  		ALU_AND		: result_o = src1_i & src2_i; 
+		ALU_OR		: result_o = src1_i | src2_i;
+		ALU_ADD		: result_o = src1_i + src2_i;
+		ALU_SUB 	: result_o = src1_i - src2_i;
+		ALU_SLT 	: result_o = signed_src1   < signed_src2   ? 32'h0001 : 32'b0;
+		ALU_SLTU 	: result_o = unsigned_src1 < unsigned_src2 ? 32'h0001 : 32'b0;
+		ALU_BNE 	: result_o = src1_i ~^ src2_i;
+		ALU_SLL 	: result_o = src2_i << shamt; 
+		ALU_SLLV 	: result_o = src2_i << src1 ; 
+		ALU_LUI 	: result_o = src2_i[15:0] << 16 ; 
+		ALU_ORI 	: result_o = src1_i | { 16'b0,src2_i[15:0]} ;
+	default: 
+		result_o = 32'b0;// No sure 
+  endcase 
+end
 
 endmodule
 
--- lab2/sourceCode/ALU_Ctrl.v
+++ lab2/sourceCode/ALU_Ctrl.v
@@ -25,9 +25,49 @@ output     [4-1:0] ALUCtrl_o;
 reg        [4-1:0] ALUCtrl_o;
 
 //Parameter
+parameter ALU_AND  = 4'd0;
+parameter ALU_OR   = 4'd1;
+parameter ALU_ADD  = 4'd2;
+parameter ALU_SUB  = 4'd3;
+parameter ALU_SLT  = 4'd4;
+parameter ALU_SLTU = 4'd5;
+parameter ALU_BNE  = 4'd6;
+parameter ALU_SLL  = 4'd7;
+parameter ALU_SLLV = 4'd8;
+parameter ALU_LUI  = 4'd9;
+parameter ALU_ORI  = 4'd10;
+parameter ALU_DONTCARE = 4'bxxxx;
 
-       
+
+parameter OP_FIELD_0  = 3'b000; 
+parameter OP_FIELD_4  = 3'b001; 
+parameter OP_FIELD_5  = 3'b010; 
+parameter OP_FIELD_8  = 3'b011; 
+parameter OP_FIELD_13 = 3'b100; 
+parameter OP_FIELD_15 = 3'b101; 
+
 //Select exact operation
+
+always @(*)begin
+    case( { ALUOp_i, funct_i } )
+        { OP_FIELD_0,  6'h00 }: ALUCtrl_o = ALU_SLL  ;
+        { OP_FIELD_0,  6'h04 }: ALUCtrl_o = ALU_SLLV ;
+        { OP_FIELD_0,  6'h20 }: ALUCtrl_o = ALU_ADD  ;
+        { OP_FIELD_0,  6'h22 }: ALUCtrl_o = ALU_SUB  ;
+        { OP_FIELD_0,  6'h24 }: ALUCtrl_o = ALU_AND  ;
+        { OP_FIELD_0,  6'h25 }: ALUCtrl_o = ALU_OR   ;
+        { OP_FIELD_0,  6'h2a }: ALUCtrl_o = ALU_SLT  ;
+        { OP_FIELD_0,  6'h2b }: ALUCtrl_o = ALU_SLTU ;
+   
+        { OP_FIELD_4,  6'h00 }: ALUCtrl_o = ALU_SUB  ;
+        { OP_FIELD_5,  6'h00 }: ALUCtrl_o = ALU_BNE  ;
+        { OP_FIELD_8,  6'h00 }: ALUCtrl_o = ALU_ADD  ;
+        { OP_FIELD_13, 6'h00 }: ALUCtrl_o = ALU_ORI  ;
+        { OP_FIELD_15, 6'h00 }: ALUCtrl_o = ALU_LUI  ;
+        default: 
+            ALUCtrl_o = ALU_DONTCARE;
+    endcase 
+end
 
 endmodule     
 
