d5ac63254bce760632556ab8ddc7b44d26456ee1
Ian Chen <ianre657@gmail.com>
2017-05-07 01:26:16 +0800

    add instructions

--- lab3/sourceCode/Decoder.v
+++ lab3/sourceCode/Decoder.v
@@ -1,48 +1,92 @@
-//Subject:     CO project 2 - ALU Controller
+//Subject:     CO project 3 - ALU Controller
 //--------------------------------------------
 //Student: 0411276 Chen Yi An
 //Student: 0413335 Kuo Yi Lin
 //--------------------------------------------
 
 module Decoder(
-    instr_op_i,
-	RegWrite_o,
-	ALU_op_o,
-	ALUSrc_o,
-	RegDst_o,
-	Branch_o
+input	[6-1:0]instr_op_i,
+output	wire 			RegWrite_o,
+		wire [4-1:0]	ALU_op_o,
+		wire			ALUSrc_o,
+		wire			RegDst_o,
+		wire			Branch_o,
+		wire			Jump_o,
+		wire [2-1:0]	MemToReg_o,
+		wire [2-1:0]	BranchType_o,
+		wire			MemRead_o,
+		wire			MemWrite_o,
 	);
-     
-//I/O ports
-input  [6-1:0] instr_op_i;
 
-output         RegWrite_o;
-output [3-1:0] ALU_op_o;
-output         ALUSrc_o;
-output         RegDst_o;
-output         Branch_o;
- 
-//Internal Signals
-reg    [3-1:0] ALU_op_o;
-reg            ALUSrc_o;
-reg            RegWrite_o;
-reg            RegDst_o;
-reg            Branch_o;
+reg [6-1:0] ALU_Reg_type;
+reg [8-1:0] Mem_Branch_type;
 
-//wire []op 
-//Parameter
+//bit count 
+//				4,			1,			1,			 1
+assign { ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } = ALU_Reg_type ;
+
+//				2,			1,			1,		  1,			2		1
+assign { MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } = Mem_Branch_J_type ;
 
 //Main function
 	always@(*)begin
 	  case (instr_op_i)
-		6'd0:  {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b000, 4'b1010 };
-		6'd4:  {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b001, 4'b00x1 };//4'b_01x1
-		6'd5:  {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b010, 4'b00x1 };
-		6'd8:  {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b011, 4'b1100 };
-		6'd13: {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b100, 4'b1100 };
-		6'd15: {  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'b101, 4'b1100 };
+	  	6'd0:begin //Add	--Representative instruction of this datapath 
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd0, 3'b011 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+		6'd2:begin // Jump
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd1, 3'bxx0 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b00x,	2'dx, 1'b1};
+		  end
+		6'd3:begin // JAL: Jump And Link
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd2, 3'bxx1 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b00x,	2'dx, 1'b1};
+		  end
+
+			//Branch 系列指令的mux都還沒搞懂意義
+		6'd4:begin //BEQ
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd3, 3'b0x0 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b001,	2'dx, 1'b0};
+		  end
+		  //Branch 系列指令的mux都還沒搞懂意義
+		6'd5:begin	//BNEZ
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd4, 3'b011 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+		  //Branch 系列指令的mux都還沒搞懂意義
+		6'd6:begin //BLT : Branch Less Than
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd5, 3'b011 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+		  //Branch 系列指令的mux都還沒搞懂意義
+		6'd7:begin //BLE : Branch Less Equal
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd6, 3'b011 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+
+		6'd8:begin //ADDI
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd7, 3'b101 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+		6'd13:begin //ORI
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd8, 3'b101 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd0, 3'b000,	2'dx, 1'b0};
+		  end
+		6'd15:begin //LI: Load immediate 
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd9, 3'b101 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd1, 3'b100,	2'dx, 1'b0};
+		  end
+		6'd35:begin //LW: Load Word
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd10, 3'b101 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'd1, 3'b100,	2'dx, 1'b0};
+		  end
+		6'd43:begin //SW: Store Word 
+		  	{ ALU_op_o, 	 ALUSrc_o, 	RegDst_o,	RegWrite_o } 	<= { 4'd11, 3'b100 };
+			{ MemToReg_o, MemRead_o, MemWrite_o,	Branch_o, BranchType_o, Jump_o } <= { 2'dx, 3'b010,	2'dx, 1'b0};
+		  end
 		default: 
-		{  ALU_op_o, RegWrite_o, ALUSrc_o, RegDst_o, Branch_o} = { 3'bxxx, 4'bxxxx };
+			{ALU_Reg_type, Mem_Branch_type} = 15'bx ; 
 	  endcase
 	end
 endmodule
